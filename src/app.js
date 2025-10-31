2cdec398312976dd3c6ca93a89109b88963f614a
messend <aquilmessen@gmail.com>
2025-10-31 21:34:10 +0000

    espero hacer algunos arreglos mas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,16 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let quien = ["Luis", "Messen", "Angela", "Bea"];
+  let accion = ["falto a", "no asistió a", "no entregó", "se fue de fiesta durante"];
+  let que = ["el trabajo", "la reunión", "los informes", "sus responsabilidades"];
+  let cuando = ["el finDeSemana", "el viernes", "el juernes", "el sabado", "el domingo"];
+
+  let quienRandom = quien[Math.floor(Math.random()* quien.length)];
+  let accionRandom = accion[Math.floor(Math.random()* accion.length)];
+  let queRandom = que[Math.floor(Math.random()* que.length)];
+  let cuandoRandom = cuando[Math.floor(Math.random()* cuando.length)];
+
+  let excuseFinal = quienRandom + " " + accionRandom + " " + queRandom + " " + cuandoRandom;
+  document.getElementById("excuse").innerHTML=excuseFinal
 };
